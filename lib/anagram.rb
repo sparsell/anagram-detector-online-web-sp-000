176d30311baf212faf244a7554d5d1f78bae9060
Sonja Parsell <hello@sparsell.com>
2020-04-27 20:22:41 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,10 +4,11 @@ class Anagram
   def initialize(word)
     @word = word
   end
-  
+
   def match(array)
     array.select do |word|
     match = @word.split("").sort == word.split("").sort
+    match
     end
   end
 end# Your code goes here!
